21fe16b4c55384c47cf7cdb010397475d6f03094
ROG <luan.roger.2003@gmail.com>
2024-02-07 10:33:29 -0300

    feat: add `User` and `Offer` table to migration

--- Migrations/20240207133237_OfferUserTable.Designer.cs
+++ Migrations/20240207133237_OfferUserTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NLWExpertAPI.Context;
 
@@ -10,9 +11,11 @@ using NLWExpertAPI.Context;
 namespace NLWExpertAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240207133237_OfferUserTable")]
+    partial class OfferUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
