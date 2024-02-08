81f7e6fb25e8aa501699a1d588731c1ac491457f
ROG <luan.roger.2003@gmail.com>
2024-02-08 15:40:04 -0300

    test: create test and cover all `UserController`

---
+++ NLWExptertAPI.Tests/ControllersTest/UserControllerTests.cs
@@ -0,0 +1,95 @@
+﻿using Moq;
+using NLWExpertAPI.Controllers;
+using NLWExpertAPI.Endpoints.RequestResponseModels;
+using NLWExpertAPI.Exceptions;
+using NLWExpertAPI.Mappers;
+using NLWExpertAPI.Mappers.Interfaces;
+using NLWExpertAPI.Mappers.RequestModelsMappers;
+using NLWExpertAPI.Models;
+using NLWExpertAPI.Models.Dto;
+using NLWExpertAPI.Repositories;
+using NLWExpertAPI.Services.Jwt;
+using NLWExpertAPI.Services.Jwt.Models;
+using NLWExptertAPI.Tests.FakeData;
+
+namespace NLWExptertAPI.Tests.ControllersTest;
+
+public class UserControllerTests
+{
+    private readonly Mock<IUserRepository> _userRepository;
+    private readonly Mock<IJwtService> _jwtService;
+    private readonly UserController _userController;
+    
+    public UserControllerTests()
+    {
+        _userRepository = new();
+        _jwtService = new();
+        IUserMapper userMapper = new UserMapper();
+        IRegisterNewUserMapper registerNewUserMapper = new RegisterNewUserMapper();
+        _userController = new(_userRepository.Object, _jwtService.Object, 
+            userMapper, registerNewUserMapper);
+    }
+
+    [Fact]
+    public async void MustRegisterNewUser()
+    {
+        RegisterNewUserRequest request = ControllersFakeData.RegisterNewUserRequest;
+        User newUser = ControllersFakeData.User;
+        _userRepository.Setup(ex => 
+                ex.CreateNewUser(It.Is<User>(user => user.email == request.email)))
+            .ReturnsAsync(newUser);
+        
+        UserDto newUserDto = await _userController.RegisterNewUser(request);
+        
+        Assert.Equal(newUser.id, newUserDto.id);
+        Assert.Equal(newUser.nome, newUserDto.nome);
+        Assert.Equal(newUser.email, newUserDto.email);
+    }
+    
+    [Fact]
+    public async void MustLoginUser()
+    {
+        LoginUserRequest request = ControllersFakeData.LoginUserRequest;
+        User user = ControllersFakeData.User;
+        const string userToken = ControllersFakeData.JWT_USER_TOKEN;
+        
+        _userRepository.Setup(ex => 
+                ex.GetUserByEmail(request.email))
+            .ReturnsAsync(user);
+        _jwtService.Setup(ex => 
+                ex.GenerateJwt(
+                    It.Is<JwtPayload>(payload => payload.userId == user.id)))
+            .Returns(userToken);
+        
+        string loginUserToken = await _userController.LoginUser(request);
+        
+        Assert.NotNull(userToken);
+        Assert.Equal(userToken, loginUserToken);
+    }
+    
+    [Fact]
+    public void MustThrowEntityNotFoundExceptionWhenUserNotFound()
+    {
+        LoginUserRequest request = ControllersFakeData.LoginUserRequest;
+        _userRepository.Setup(ex => 
+                ex.GetUserByEmail(request.email))
+            .ReturnsAsync(() => null);
+        
+        Assert.ThrowsAsync<EntityNotFoundException>(() => 
+            _userController.LoginUser(request));
+    }
+    
+    [Fact]
+    public void MustThrowWrongUserPasswordExceptionWhenPasswordIsWrong()
+    {
+        LoginUserRequest request = ControllersFakeData.LoginUserRequest;
+        User user = ControllersFakeData.User;
+        user.senha = "wrongPassword";
+        _userRepository.Setup(ex => 
+                ex.GetUserByEmail(request.email))
+            .ReturnsAsync(user);
+        
+        Assert.ThrowsAsync<WrongUserPasswordException>(() => 
+            _userController.LoginUser(request));
+    }
+}
---
+++ NLWExptertAPI.Tests/FakeData/ControllersFakeData.cs
@@ -0,0 +1,29 @@
+﻿using NLWExpertAPI.Endpoints.RequestResponseModels;
+using NLWExpertAPI.Models;
+
+namespace NLWExptertAPI.Tests.FakeData;
+
+public static class ControllersFakeData
+{
+    public static RegisterNewUserRequest RegisterNewUserRequest => new()
+    {
+        nome = "test",
+        email = "test@test.com",
+        senha = "test"
+    };
+
+    public static User User => new()
+    {
+        nome = "test",
+        email = "test@test.com",
+        senha = "test123"
+    };
+
+    public static LoginUserRequest LoginUserRequest => new()
+    {
+        email = "test@test.com",
+        senha = "test123"
+    };
+
+    public const string JWT_USER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEiLCJuYmYiOjE3MDc0MTQzMDQsImV4cCI6MTcwNzQzMjMwNCwiaWF0IjoxNzA3NDE0MzA0LCJpc3MiOiJOTFdFeHBlcnRBUEkiLCJhdWQiOiJOTFdFeHBlcnRGcm9udGVuZCJ9.8rk1KCJziLtCL9QsY4vB8KR9cd7KqbpJ4iIfjhPf16Q";
+}
