f17d235aaf7db2a704e62038be8c1cf7ee60a37a
ROG <luan.roger.2003@gmail.com>
2024-02-08 14:44:13 -0300

    refactor: move the DI services to file

--- Program.cs
+++ Program.cs
@@ -1,62 +1,20 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using NLWExpertAPI.Context;
-using NLWExpertAPI.Controllers;
 using NLWExpertAPI.Endpoints;
-using NLWExpertAPI.Exceptions;
-using NLWExpertAPI.Mappers;
-using NLWExpertAPI.Mappers.Interfaces;
-using NLWExpertAPI.Mappers.RequestModelsMappers;
-using NLWExpertAPI.Models;
-using NLWExpertAPI.Repositories;
+using NLWExpertAPI.Services;
 using NLWExpertAPI.Services.Auth;
-using NLWExpertAPI.Services.Jwt;
-using NLWExpertAPI.Utils;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    string? connectionString = EnvVars.GetSqliteConnectionString();
-    if(connectionString is null)
-        throw new EnvironmentVariableIsNullOrEmptyException(nameof(EnvVars.SQLITE_CONNECTION_STRING));
-    
-    options.UseSqlite(connectionString);
-});
-
-builder.Services.Configure<JwtParametersOption>(builder
-    .Configuration
-    .GetSection(JwtParametersOption.JWT_PARAMETERS));
-
-builder.Services.AddTransient<IConfigureOptions<JwtBearerOptions>, AuthenticationJwtBearerConfiguration>();
-builder.Services.AddSingleton<IJwtService, JwtService>();
-
-builder.Services.AddScoped<IItemMapper, ItemMapper>();
-builder.Services.AddScoped<IAuctionMapper, AuctionMapper>();
-builder.Services.AddScoped<IOfferMapper, OfferMapper>();
-builder.Services.AddScoped<IUserMapper, UserMapper>();
-builder.Services.AddScoped<IRegisterNewUserMapper, RegisterNewUserMapper>();
-builder.Services.AddScoped<INewItemForAuctionMapper, NewItemForAuctionMapper>();
-builder.Services.AddScoped<ICreateNewAuctionMapper, CreateNewAuctionMapper>();
-
-builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
-builder.Services.AddScoped<IOfferRepository, OfferRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-builder.Services.AddScoped<IAuctionController, AuctionController>();
-builder.Services.AddScoped<IOfferController, OfferController>();
-builder.Services.AddScoped<IUserController, UserController>();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer();
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy(AuthorizationPolicies.REQUIRE_IDENTIFIER, 
-        AuthorizationPolicies.RequireIdentifierAndUserRole);
-});
+builder.Services
+    .ConfigureServices(builder.Configuration)
+    .AddConfiguration()
+    .AddSqliteContext()
+    .AddMappers()
+    .AddRepositories()
+    .AddCustomControllers()
+    .AddAuth()
+    .AddSwaggerDependencies();
 
 WebApplication app = builder.Build();
 
@@ -66,7 +24,6 @@ if(app.Environment.IsDevelopment())
     app.UseSwaggerUI();
     using IServiceScope serviceScope = app.Services.CreateScope();
     AppDbContext context = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
-    context.Database.EnsureCreated();
     context.Database.Migrate();
 }
 
---
+++ Services/Di.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NLWExpertAPI.Context;
+using NLWExpertAPI.Controllers;
+using NLWExpertAPI.Exceptions;
+using NLWExpertAPI.Mappers;
+using NLWExpertAPI.Mappers.Interfaces;
+using NLWExpertAPI.Mappers.RequestModelsMappers;
+using NLWExpertAPI.Models;
+using NLWExpertAPI.Repositories;
+using NLWExpertAPI.Services.Auth;
+using NLWExpertAPI.Services.Jwt;
+using NLWExpertAPI.Utils;
+
+namespace NLWExpertAPI.Services;
+
+public static class Di
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services, 
+        IConfigurationManager configuration)
+    {
+        services.Configure<JwtParametersOption>(
+            configuration.GetSection(JwtParametersOption.JWT_PARAMETERS));
+
+        return services;
+    }
+    public static IServiceCollection AddConfiguration(this IServiceCollection services)
+    {
+        services.AddTransient<IConfigureOptions<JwtBearerOptions>, AuthenticationJwtBearerConfiguration>();
+        services.AddSingleton<IJwtService, JwtService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddSqliteContext(this IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            string? connectionString = EnvVars.GetSqliteConnectionString();
+            if(connectionString is null)
+                throw new EnvironmentVariableIsNullOrEmptyException(nameof(EnvVars.SQLITE_CONNECTION_STRING));
+    
+            options.UseSqlite(connectionString);
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddMappers(this IServiceCollection services)
+    {
+        services.AddScoped<IItemMapper, ItemMapper>();
+        services.AddScoped<IAuctionMapper, AuctionMapper>();
+        services.AddScoped<IOfferMapper, OfferMapper>();
+        services.AddScoped<IUserMapper, UserMapper>();
+        services.AddScoped<IRegisterNewUserMapper, RegisterNewUserMapper>();
+        services.AddScoped<INewItemForAuctionMapper, NewItemForAuctionMapper>();
+        services.AddScoped<ICreateNewAuctionMapper, CreateNewAuctionMapper>();
+
+        return services;
+    }
+    
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IAuctionRepository, AuctionRepository>();
+        services.AddScoped<IOfferRepository, OfferRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddCustomControllers(this IServiceCollection services)
+    {
+        services.AddScoped<IAuctionController, AuctionController>();
+        services.AddScoped<IOfferController, OfferController>();
+        services.AddScoped<IUserController, UserController>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddAuth(this IServiceCollection services)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer();
+        services.AddAuthorizationBuilder()
+            .AddPolicy(AuthorizationPolicies.REQUIRE_IDENTIFIER, 
+                AuthorizationPolicies.RequireIdentifierAndUserRole);
+
+        return services;
+    }
+
+    public static IServiceCollection AddSwaggerDependencies(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+}
